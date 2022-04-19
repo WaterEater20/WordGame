d07b82ba316779f90b9c95a8539cb631d11016ac
Gjon Bicaj <95306441+WaterEater20@users.noreply.github.com>
2022-04-19 16:09:29 +0000

    New

--- Main.py
+++ Main.py
@@ -3,12 +3,13 @@ data=[
     "words":[{"title":"Europe","clues":["Paris","Euro","Polish Leader I think"] 
              }
             ]
-    },{"title":"Geography",
-    "words":[{"title":"Europe","clues":["Paris","Euro","Polish Leader I think"] 
-             },{"title":"Europe","clues":["Paris","Euro","Polish Leader I think"] 
+    },{"title":"School",
+    "words":[{"title":"Homework","clues":["School at home","Due tomorrow","At your own pace"] 
+             },{"title":"Fortnite","clues":["Boogie","Ninja","Tfue"] 
              }
             ]
-    }
+    },{"title":"History",
+    "words":[{titil}]
 ]
 
 print(data)
