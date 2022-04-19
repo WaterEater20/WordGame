db25f2986b626f3810f8a9c6e175d57f2432f05a
Gjon Bicaj <95306441+WaterEater20@users.noreply.github.com>
2022-04-19 16:42:35 +0000

    Ev

--- Main.py
+++ Main.py
@@ -2,14 +2,25 @@ data=[
     {"title":"Geography",
     "words":[{"title":"Europe","clues":["Paris","Euro","Polish Leader I think"] 
              }
-            ]
-    },{"title":"School",
+            ]},
+    {"title":"School",
     "words":[{"title":"Homework","clues":["School at home","Due tomorrow","At your own pace"] 
              },{"title":"Fortnite","clues":["Boogie","Ninja","Tfue"] 
              }
             ]
-    },{"title":"History",
-    "words":[{titil}]
+    },{"title":"School",
+    "words":[{"title":"Homework","clues":["School at home","Due tomorrow","At your own pace"] 
+             },{"title":"Fortnite","clues":["Boogie","Ninja","Tfue"]}]}
+              
 ]
 
-print(data)
+#print(data)
+
+guessing=True
+while guessing:
+  hidden = data[0]["words"][0]["title"]
+  guess = input("what is your guess ")
+  if guess == hidden:
+    print("cool")
+    guessing=False
+    
