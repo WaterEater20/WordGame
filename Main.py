5abfa0c557cde9c3088e4d7c130ce8cd787b5907
Gjon Bicaj <95306441+WaterEater20@users.noreply.github.com>
2022-04-29 13:42:46 +0000

    Loop

--- Main.py
+++ Main.py
@@ -8,19 +8,29 @@ data=[
              },{"title":"Fortnite","clues":["Boogie","Ninja","Tfue"] 
              }
             ]
-    },{"title":"School",
-    "words":[{"title":"Homework","clues":["School at home","Due tomorrow","At your own pace"] 
-             },{"title":"Fortnite","clues":["Boogie","Ninja","Tfue"]}]}
-              
+    },{"title":"History",
+    "words":[{"title":"Italy","clues":["Roman Empire","Romanum Empirum","All roads lead to Rome"] 
+             },{"title":"The United States","clues":["July 4th 1776","North American Powerhouse","Dollar"] 
+             }
+            ]
+      }
 ]
+
 
 #print(data)
 
 guessing=True
+#rannum1
 while guessing:
   hidden = data[0]["words"][0]["title"]
-  guess = input("what is your guess ")
-  if guess == hidden:
-    print("cool")
-    guessing=False
-    
+  print ("You're category is",data[0]["title"])
+  for i in data[0]["words"][0]["clues"]:
+    print ("You're first clue is",i)
+    guess = input ("Geography what is your guess")
+    if guess == hidden: 
+      print("cool")
+      guessing=False
+
+import random
+
+print(random.randrange(3, 9))
